ec7a22fd3e1a384b874acc4a2c05989e652d48e8
daichi_sato <daichiexperience@gmail.com>
2022-10-19 23:13:10 +0900

    [add] config

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+/* eslint @typescript-eslint/naming-convention: 0 */
+export const CMS_API_TOKEN = process.env.NEXT_PUBLIC_CMS_API_TOKEN as string;
+export const CMS_API_URL = process.env.NEXT_PUBLIC_CMS_API_URL as string;
