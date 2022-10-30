a1a78049cb40139ed3a55396de1bdee42d0c820f
daichi_sato <daichiexperience@gmail.com>
2022-10-30 12:41:42 +0900

    [add] micro-cms-sdk

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 /* eslint @typescript-eslint/naming-convention: 0 */
-export const CMS_API_TOKEN = process.env.NEXT_PUBLIC_CMS_API_TOKEN as string;
-export const CMS_API_URL = process.env.NEXT_PUBLIC_CMS_API_URL as string;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL as string;
