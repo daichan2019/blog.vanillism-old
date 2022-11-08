875c2871224d4b4193816b858a9384e2b77a5ac2
daichi_sato <daichiexperience@gmail.com>
2022-11-08 23:32:24 +0900

    wip

--- src/components/model/blog/BlogCard/BlogCard.tsx
+++ src/components/model/blog/BlogCard/BlogCard.tsx
@@ -10,13 +10,21 @@ type Props = {
 
 export const BlogCard: FC<Props> = ({ blog }) => {
   return (
-    <div className='max-w-sm rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800'>
-      <Link
-        className='flex flex-col items-center rounded-lg border bg-white shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row'
-        href={`/articles/${blog.id}`}
-      >
-        <Image src={blog.eyecatch.url} alt='' width={30} height={20} />
+    <div className='flex flex-col items-center rounded-lg border bg-white shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row'>
+      <Link href={`/articles/${blog.id}`}>
+        <div className='relative aspect-video rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'>
+          <Image src={blog.eyecatch.url} alt='' layout='fill' />
+        </div>
       </Link>
+      <div className='flex flex-1 flex-col justify-between p-4 leading-normal'>
+        <h5 className='font-bold tracking-tight text-gray-900 dark:text-white md:text-2xl'>
+          {blog.title}
+        </h5>
+        <p className='mt-3 font-normal text-gray-700 dark:text-gray-400'>
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+          chronological order.
+        </p>
+      </div>
     </div>
   );
 };
