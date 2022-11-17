45521a44097f9aa053cb987696f128a7e16f43b3
daichi_sato <daichiexperience@gmail.com>
2022-11-18 00:02:43 +0900

    [add] code hilight

--- src/components/page/Article/Article.tsx
+++ src/components/page/Article/Article.tsx
@@ -1,43 +1,37 @@
 import type { FC } from 'react';
 import { formatDate } from 'src/utils/formatDate';
 
-import { Header } from '@/components/layout/Header';
+import { DefaultLayout } from '@/components/layout/DefaultLayout';
 import { Tag } from '@/components/model/tag/Tag';
 import type { EndPoints } from '@/lib/cms/types';
 
 type Props = {
   blog: EndPoints['get']['blogs'];
+  highlightedBody: string;
 };
 
-export const Article: FC<Props> = ({ blog }) => {
+export const Article: FC<Props> = ({ blog, highlightedBody }) => {
   return (
-    <div className='bg-neutral-50'>
-      <Header />
-      <div className='py-12'>
-        <div className='container m-auto flex max-w-5xl flex-1'>
-          <main>
-            <h2 className='my-2 text-2xl font-extrabold leading-9 tracking-tight sm:my-3 sm:text-3xl sm:leading-10 md:my-4 md:text-4xl'>
-              {blog.title}
-            </h2>
-            <time className='text-gray-600' dateTime={blog.createdAt}>
-              {formatDate(blog.createdAt)}
-            </time>
-            <ul className='my-2 flex gap-2'>
-              {blog.tags.map((tag) => {
-                return (
-                  <li key={tag.id}>
-                    <Tag tag={tag} />
-                  </li>
-                );
-              })}
-            </ul>
-            <div
-              className='prose pt-7 text-black'
-              dangerouslySetInnerHTML={{ __html: `${blog.content}` }}
-            ></div>
-          </main>
-        </div>
-      </div>
-    </div>
+    <DefaultLayout>
+      <h2 className='my-2 text-2xl font-extrabold leading-9 tracking-tight sm:my-3 sm:text-3xl sm:leading-10 md:my-4 md:text-4xl'>
+        {blog.title}
+      </h2>
+      <time className='text-gray-600' dateTime={blog.createdAt}>
+        {formatDate(blog.createdAt)}
+      </time>
+      <ul className='my-2 flex gap-2'>
+        {blog.tags.map((tag) => {
+          return (
+            <li key={tag.id}>
+              <Tag tag={tag} />
+            </li>
+          );
+        })}
+      </ul>
+      <div
+        className='prose pt-7 text-black max-w-none'
+        dangerouslySetInnerHTML={{ __html: highlightedBody }}
+      ></div>
+    </DefaultLayout>
   );
 };
--- src/pages/articles/[id]/index.tsx
+++ src/pages/articles/[id]/index.tsx
@@ -1,3 +1,7 @@
+import 'highlight.js/styles/vs2015.css';
+
+import cheerio from 'cheerio';
+import hljs from 'highlight.js';
 import type { NextPage } from 'next';
 import type { InferGetStaticPropsType } from 'next';
 import type { GetStaticPaths, GetStaticProps } from 'next';
@@ -7,8 +11,8 @@ import { client } from '@/lib/cms/utils';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-const ArticlePage: NextPage<Props> = ({ blog }) => {
-  return <Article blog={blog} />;
+const ArticlePage: NextPage<Props> = ({ blog, highlightedBody }) => {
+  return <Article blog={blog} highlightedBody={highlightedBody} />;
 };
 
 export default ArticlePage;
@@ -28,11 +32,21 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const id = params?.id;
-  const data = await client.blogs._id(id as string).$get();
+  const blog = await client.blogs._id(id as string).$get();
+
+  const $ = cheerio.load(blog.content || '');
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  $('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass('hljs');
+  });
 
   return {
     props: {
-      blog: data,
+      blog,
+      highlightedBody: $.html(),
     },
   };
 };
