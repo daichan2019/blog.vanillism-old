6f2a35b27b4711b8d5baf812a09e6f75f7178d10
daichi_sato <daichiexperience@gmail.com>
2022-11-28 22:43:38 +0900

    [add] articles page

--- src/components/page/Articles/Articles.tsx
+++ src/components/page/Articles/Articles.tsx
@@ -13,6 +13,9 @@ type Props = {
 export const Articles: FC<Props> = ({ blogs, totalCount }) => {
   return (
     <DefaultLayout>
+      <h2 className='border-b-[1px] md:leading-10 text-3xl font-extrabold sm:text-4xl sm:leading-10 md:text-6xl pb-6'>
+        Articles
+      </h2>
       <ul className='divide-y divide-gray-200'>
         {blogs.map((blog) => {
           return (
