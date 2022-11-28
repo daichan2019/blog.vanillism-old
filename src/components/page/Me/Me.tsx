50667a40aa2e4c3506be6769920409c6f41a703f
daichi_sato <daichiexperience@gmail.com>
2022-11-28 12:59:28 +0900

    [wip] me

--- src/components/page/Me/Me.tsx
+++ src/components/page/Me/Me.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 
 import { DefaultLayout } from '@/components/layout/DefaultLayout';
+import { SocialIcon } from '@/components/ui/ui-elements/SocialIcon';
 import type { EndPoints } from '@/lib/cms/types';
 
 type Props = {
@@ -8,9 +9,24 @@ type Props = {
 };
 
 export const Me: FC<Props> = ({ author }) => {
+  const { socialMedias } = author;
+  console.log(socialMedias[0].name[0]);
+
   return (
     <DefaultLayout>
-      <div>{author.name}</div>
+      <h2>About Me</h2>
+      <div>
+        <p>{author.name}</p>
+        <ul className='mb-3 flex space-x-4'>
+          {socialMedias.map((social) => {
+            return (
+              <li key={social.name[0]}>
+                <SocialIcon kind={social.name[0]} href={social.url} size='sm' />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </DefaultLayout>
   );
 };
