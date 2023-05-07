8a2347f0f4df5853377e5d744977da3cb9c1e649
daichi_sato <daichiexperience@gmail.com>
2023-05-07 18:37:17 +0900

    feat: update fallback option to blocking

--- src/pages/tags/[id]/page/[num]/index.tsx
+++ src/pages/tags/[id]/page/[num]/index.tsx
@@ -56,7 +56,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   const paths = resPaths.flat();
 
-  return { paths, fallback: false };
+  return { paths, fallback: 'blocking' };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
